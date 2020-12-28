5928bf4b62252cb9f52be03239a31836effe30b5
Ethan B <eebmagic@gmail.com>
2020-12-28 00:01:16 -0600

    Small features
    App
    - App now has a state
        - state has an array of Objects
    
    components/
    - Make Todos object in components/
        - Takes an argument: items
        - items are rendered in Todos object

--- src/App.js
+++ src/App.js
@@ -1,11 +1,44 @@
+import React, { Component} from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  );
+// A class definition that extends react.Component
+class App extends Component {
+  // Data for App class
+  // State is a js object
+  state = {
+    // Var with name todos that is an array of objects
+    todos: [
+      {
+        id: 1,
+        title: 'Take out the trash',
+        completed: false
+      },
+      {
+        id: 2,
+        title: 'Make dinner',
+        completed: false
+      },
+      {
+        id: 3,
+        title: 'Homework',
+        completed: false
+      }
+    ]
+  }
+
+  // How the data of App is rendered and returned to the index.html
+  // rendering an app involves a Todo object
+  // with an items argument as the App state
+  render() {
+    console.log(this.state.todos)
+    return (
+      <div className="App">
+        <h1>Hello World!</h1>
+        <Todos items={this.state.todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Todos.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+
+class Todos extends Component {
+    render() {
+        // console.log('Todos object created with items arg:')
+        // console.log(this.props.items);
+        return (
+            <div className="Todos">
+            <h2>Todo Object</h2>
+            </div>
+        );
+    }
+}
+
+export default Todos;
